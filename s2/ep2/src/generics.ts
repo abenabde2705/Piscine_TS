f6000d0ca05b95738500a7b9b7bd9674506326e2
Amrou Ben Abdessalem <amroubenabdessalem06@gmail.com>
2025-02-26 14:24:38 +0100

    ep2 finished

--- s2/ep2/src/generics.ts
+++ s2/ep2/src/generics.ts
@@ -1,10 +1,13 @@
-function premierElement<T>(tableau: T[]):T | null {
-    return tableau[0];
+
+function premierElement<T>(tableau: T[]):T | string {
+    return tableau.length > 0 ? tableau[0] : "error le tableau est vide";
   }
 
 let numbers: number[] = [1, 2, 3];
 let strings: string[] = ['a', 'b', 'c'];
 let booleans: boolean[] = [true, false, true];
+let vide: boolean[] = [];
 console.log(premierElement(numbers));
 console.log(premierElement(strings)); 
 console.log(premierElement(booleans));
+console.log(premierElement(vide));
