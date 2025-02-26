df78fcb730bc6e1be76c5362a0f35d8f845b16d2
Amrou Ben Abdessalem <amroubenabdessalem06@gmail.com>
2025-02-26 14:59:20 +0100

    ep1 s3 finished

---
+++ s3/ep1/src/narrowing.ts
@@ -0,0 +1,11 @@
+function traiterValeur(params:string|number) {
+
+  if(typeof params ==="string"){
+    return params.length;
+  }else{
+    return params*1;
+  }
+}
+
+console.log(traiterValeur("hello"));
+console.log(traiterValeur(123));
